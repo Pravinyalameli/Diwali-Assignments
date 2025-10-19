5fc8f4856b746635e67debb6df194b2480d1c7bc
Pravinyalameli <126939794+Pravinyalameli@users.noreply.github.com>
2025-10-19 12:09:20 +0530

    Delete DiwaliAssignments directory

--- DiwaliAssignments/src/com/demo/dao/StudentDao.java
+++
@@ -1,9 +0,0 @@
-package com.demo.dao;
-
-import com.demo.model.Student;
-import java.util.List;
-
-public interface StudentDao {
-    void saveStudentsToFile(List<Student> students, String filename);
-    List<Student> readStudentsFromFile(String filename);
-}
--- DiwaliAssignments/src/com/demo/model/LowAttendanceException.java
+++
@@ -1,7 +0,0 @@
-package com.demo.model;
-
-public class LowAttendanceException extends Exception {
-    public LowAttendanceException(String message) {
-        super(message);
-    }
-}
